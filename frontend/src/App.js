5b9eaf7c30a807d0ab7d286ea049c2c6dbabd246
rudynurafif <rudynurafif@gmail.com>
2023-09-19 21:45:37 +0700

    Starting the frontend with home screen and product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <h1>Welcome to RANShop</h1>
+  return (
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
